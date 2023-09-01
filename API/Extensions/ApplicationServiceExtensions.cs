bb30a245701d91fd6888479c602c111fdfeb3cd4
John Ubaque <jubaque@sb.gob.do>
2023-09-01 17:22:52 -0500

    final seccion 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Application;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddDbContext<DataContext>(opt=>
+        {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors(opt=>{
+            opt.AddPolicy("CorsPolicy", policy=>{
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+        return services;
+
+    }
+}
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using Persistence;
+
+namespace Application;
+
+public class Delete
+{
+    public class  Command: IRequest
+    {
+        public Guid Id {get; set;}
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+           private readonly DataContext _Context ;
+        public Handler(DataContext context)
+        {
+            _Context = context;
+        }
+
+     
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var Activity = await _Context.Activities.FindAsync(request.Id);
+            _Context.Remove(Activity);
+            await _Context.SaveChangesAsync();
+
+        }
+    }
+
+}
