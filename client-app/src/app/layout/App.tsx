06b400c315863ee508d353d73ab65e4bede14bd8
John Ubaque <jubaque@sb.gob.do>
2023-09-11 18:36:15 -0500

    fin de seccion 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,84 @@
+
+import {  useEffect, useState } from 'react'
+
+import axios from 'axios';
+import {  Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import Navbar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid'
+
+function App() {
+
+  const [activities,setActivities]= useState<Activity[]>([]);
+  const [selectedActivity,setSelecterActivity] = useState<Activity|undefined>(undefined);
+  const [editMode,setEditMode]= useState(false);
+
+
+
+  useEffect(()=>{
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+      .then(Response=>{
+          setActivities(Response.data)
+      })
+  },[])
+
+  function handleSelectActivity(id:string){
+  setSelecterActivity(activities.find(x=>x.id===id));
+  }
+
+  function cancelSelectActivity(){
+    setSelecterActivity(undefined);
+    }
+
+  function handleFormOpen(id?:string)
+  {
+    id? handleSelectActivity(id): cancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity (activity: Activity)
+  {
+    activity.id?
+     setActivities([...activities.filter(x=>x.id!==activity.id),activity])
+     : setActivities([...activities,{...activity,id: uuid()}]);
+
+     setEditMode(false);
+     setSelecterActivity(activity);
+  }
+
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x=>x.id!==id)])
+  }
+
+
+  return (
+    <>
+      <Navbar openForm={handleFormOpen}/>
+
+    <Container style= {{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities} 
+        selectedActivity = {selectedActivity}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity= {cancelSelectActivity}
+        editMode={editMode}
+        openForm={handleFormOpen}
+        closeForm = {handleFormClose}
+        createOrEdit = {handleCreateOrEditActivity}
+        deleteActivity = {handleDeleteActivity}
+        />
+
+    </Container>
+   
+
+    </>
+
+  )
+}
+
+export default App
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,61 @@
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { ChangeEvent, useState } from "react";
+
+
+
+interface Props{
+    activity: Activity|undefined;  
+    closeForm: ()=>void;
+    createOrEdit: (activity:Activity)=>void;
+
+
+
+}
+
+export default function ActivityForm({activity:selectedActivity,closeForm,createOrEdit}:Props) {
+
+const initialState=selectedActivity ?? {
+  id:'',
+  title: '',
+  category: '',
+  description : '',
+  date: '',
+  city:'',
+  venue:''
+
+} 
+
+const [activity,setActivity] = useState(initialState);
+
+function handleSubmit(){
+  createOrEdit(activity);
+}
+
+function handleInputChange(event: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>){
+
+const {name,value} = event.target;
+
+setActivity({...activity,[name]:value});
+
+}
+
+
+
+  return (
+    <Segment clearing>   
+        <Form onSubmit={handleSubmit} autoComplete= 'off' onChange= {handleInputChange}>
+                <Form.Input placeHolder = 'title'  name='title' value = {activity.title}  onChange={handleInputChange}/>
+                <Form.TextArea placeHolder = 'description'   name='description'    value = {activity.description}  onChange={handleInputChange}/>
+                <Form.Input placeHolder = 'category'  name='category' value = {activity.category}  onChange={handleInputChange}/>
+                <Form.Input placeHolder = 'date' name='date' value = {activity.date}  onChange={handleInputChange}/>
+                <Form.Input placeHolder = 'city' name='city' value = {activity.city}  onChange={handleInputChange}/>
+                <Form.Input placeHolder = 'venue' name='venue' value = {activity.venue}  onChange={handleInputChange} />
+                <Button floated = 'right' positive type ='submit' content = 'submit' />
+                <Button onClick={closeForm} floated = 'right' positive type ='button' content = 'Cancel' />
+
+        </Form>
+
+    </Segment>     
+  )
+}
